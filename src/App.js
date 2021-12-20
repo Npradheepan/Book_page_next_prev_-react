ce35823330d2b8f99dbb67b4cdf29f433e8a89fd
pradh <pradh@DESKTOP-765MFUR>
2021-12-20 15:13:42 +0100

    initial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+import axios from 'axios';
+import Pagination from './Pagination';
+
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentUrl]= useState("")
+  const[nextPageUrl, setnextPageUrl]= useState()
+  const  [prevPageurl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+useEffect(()=> {
+  setLoading(true)
+  let cencel
+  axios.get(currentPageUrl, {cancelToken:new axios.cancelToken(c=>cancel = c)
+  
+  }).then(res => {
+    setLoading(false)
+    setnextPageUrl(res.data.next)
+    setPrevPageUrl(res.data.previous)
+    setPokemon(res.data.results.map(p=>p.name))
+  })
+  return ()=> cancel()
+}, [currentPageUrl]);
+
+function gotoNextPage() {
+  setCurrentPageUrl(nextPageUrl)
+}
+
+function gotoPrevPage() {
+  setCurrentPageUrl(prevPageurl)
+}
+
+if(loading)return"Loading..."
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PokemonList pokemon = {pokemon} />
+      <PokemonList
+      gotoNextPage = {nextPageUrl? gotoNextPage: null}
+      gotoNextPage = {prevPageUrl? gotoPrevPage: null}
+      />
     </div>
   );
 }
---
+++ src/PokemonLists.js
@@ -0,0 +1,11 @@
+import React  from "react";
+
+export default function PokemonList({pokemon}) {
+    return(
+        <div>
+            {pokeman.man(p =>(
+                <div key={p}>{p}</div>
+            ))}
+        </div>
+    )
+}
